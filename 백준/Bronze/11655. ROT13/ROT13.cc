28a1c7fe482a35d6a15883088a65eb850d24610a
Ko MinKyung <103225693+komg00@users.noreply.github.com>
2025-01-31 23:06:51 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-string s, ret;
+string s;
+
 int main() {
 	getline(cin, s);
-	for(int i = 0; i < s.length(); i++) {
-		if (97 <= s[i] && s[i] <= 122) {  // 소문자 
-			if(s[i] + 13 > 122) {
-				cout << (char)(s[i] - 26 + 13);
-			}
-			else
-				cout << (char)(s[i] + 13);
+	
+	for(int i = 0; i < s.size(); i++) {
+		int k = s[i];
+		
+		if((k >= 65 && k <= 77) || (k >= 97 && k <= 109)) {
+			cout << (char)(s[i] + 13);
 		}
-		else if (65 <= s[i] && s[i] <= 90) { // 대문자 
-			if(s[i] + 13 > 90) {
-				cout << (char)(s[i] - 26 + 13);
-			}
-			else
-				cout << (char)(s[i] + 13);
+		else if((k >= 78 && k <= 90) || (k >= 110 && k <= 122)) {
+			cout << (char)(s[i] - 13);
 		}
-		else {
-			cout << s[i];
-		}
+		else cout << s[i];
 	}
 }
